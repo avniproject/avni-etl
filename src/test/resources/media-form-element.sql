6b7ff69c8c6dbc9c9caaa751d766633ea80a3a10
Joy A <itsjoy@gmail.com>
2024-10-01 15:32:05 +0530

    #0 | Remove form_element_group.display from test/resources sql inserts

--- src/test/resources/media-form-element.sql
+++ src/test/resources/media-form-element.sql
@@ -8,7 +8,7 @@ update individual set observations = observations || jsonb_build_object('b181501
 
 
 -- Encounter
-insert into form_element_group (id, name, form_id, uuid, version, display_order, display, organisation_id, audit_id, is_voided, rule, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time, declarative_rule) values (50571, 'Page', 2660, 'c28ba4eb-f159-4b18-a7c1-baf23d6f7259', 0, 10, 'Page', 12, create_audit(), false, null, 1, 1, '2022-03-16 15:51:44.666 +00:00', '2022-03-16 15:51:44.666 +00:00', null);
+insert into form_element_group (id, name, form_id, uuid, version, display_order, organisation_id, audit_id, is_voided, rule, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time, declarative_rule) values (50571, 'Page', 2660, 'c28ba4eb-f159-4b18-a7c1-baf23d6f7259', 0, 10, 12, create_audit(), false, null, 1, 1, '2022-03-16 15:51:44.666 +00:00', '2022-03-16 15:51:44.666 +00:00', null);
 insert into form_element (id, name, display_order, is_mandatory, key_values, concept_id, form_element_group_id, uuid, version, organisation_id, type, valid_format_regex, valid_format_description_key, audit_id, is_voided, rule, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time, declarative_rule) values (50115, 'Media form element', 1, false, '[]', 108001, 50571, '3ae42c9b-136f-40da-bd9b-2ff638e55683', 0, 12, 'SingleSelect', null, null, create_audit(), false, null, 1, 1, '2022-04-13 10:51:44.705 +00:00', '2022-04-13 10:51:44.705 +00:00', null);
 
 
