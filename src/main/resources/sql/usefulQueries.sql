e2bcc90f7a1329feddb20c43f2b5dd65b0c914e1
himeshr <himeshl@samanvayfoundation.org>
2025-02-28 16:45:09 +0530

    #124 | Update delete_etl_metadata_for_schema and delete_etl_metadata_for_org to handle schemaNames with hiphen

--- src/main/resources/sql/usefulQueries.sql
+++ src/main/resources/sql/usefulQueries.sql
@@ -11,15 +11,15 @@ ORDER BY routines.routine_name, parameters.ordinal_position;
 select * from information_schema.routines where routines.routine_name= 'delete_etl_metadata_for_schema';
 select * from pg_proc where proname= 'delete_etl_metadata_for_schema';
 
--- Create function to delete etl metadata for an org
-DROP FUNCTION IF EXISTS delete_etl_metadata_for_schema(inrolname text, inpassword text);
-create function delete_etl_metadata_for_schema(in_impl_schema text, in_db_user text) returns bool
+-- Create or replace function to delete etl metadata for an org
+DROP FUNCTION IF EXISTS delete_etl_metadata_for_schema;
+create function delete_etl_metadata_for_schema(in_impl_schema text, in_db_user text, in_db_owner text) returns bool
     language plpgsql
 as
 $$
 BEGIN
-    EXECUTE 'set role ' || in_impl_schema || ';';
-    execute 'drop schema ' || in_impl_schema || ' cascade;';
+    execute 'set role "' || in_db_owner || '";';
+    execute 'drop schema if exists "' || in_impl_schema || '" cascade;';
     execute 'delete from entity_sync_status where db_user = ''' || in_db_user || ''';';
     execute 'delete from entity_sync_status where schema_name = ''' || in_impl_schema || ''';';
     execute 'delete from index_metadata where table_metadata_id in (select id from table_metadata where schema_name = ''' || in_impl_schema || ''');';
@@ -30,14 +30,14 @@ END
 $$;
 
 -- Create function to delete etl metadata for org-group
-DROP FUNCTION IF EXISTS delete_etl_metadata_for_org(inrolname text, inpassword text);
+DROP FUNCTION IF EXISTS delete_etl_metadata_for_org;
 create function delete_etl_metadata_for_org(in_impl_schema text, in_db_user text) returns bool
     language plpgsql
 as
 $$
 BEGIN
     EXECUTE 'set role openchs;';
-    execute 'drop schema ' || in_impl_schema || ' cascade;';
+    execute 'drop schema "' || in_impl_schema || '" cascade;';
     execute 'delete from entity_sync_status where db_user = ''' || in_db_user || ''';';
     execute 'delete from entity_sync_status where schema_name = ''' || in_impl_schema || ''';';
     execute 'delete from index_metadata where table_metadata_id in (select id from table_metadata where schema_name = ''' || in_impl_schema || ''');';
