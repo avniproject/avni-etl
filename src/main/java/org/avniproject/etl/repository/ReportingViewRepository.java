4109597299d2426f9169e8b401607185f012014a
Vivek Singh <petmongrels@gmail.com>
2025-03-05 12:41:40 +0530

    #123 - renamed the views

--- src/main/java/org/avniproject/etl/repository/ReportingViewRepository.java
+++ src/main/java/org/avniproject/etl/repository/ReportingViewRepository.java
@@ -13,11 +13,11 @@ import java.util.List;
 import static org.avniproject.etl.repository.sql.SqlFile.readFile;
 
 @Repository
-public class ReportingViewRepository  implements ReportingViewMetaData{
+public class ReportingViewRepository implements ReportingViewMetaData {
     private static final Logger log = Logger.getLogger(ReportingViewRepository.class);
     private final JdbcTemplate jdbcTemplate;
-    private final String individualViewFile = readFile("/sql/etl/view/individualAddressView.sql.st");
-    private final String enrolmentViewFile = readFile("/sql/etl/view/enrolmentAddressView.sql.st");
+    private final String subjectViewFile = readFile("/sql/etl/view/subjectView.sql.st");
+    private final String enrolmentViewFile = readFile("/sql/etl/view/enrolmentView.sql.st");
     private final String grantViewFile = readFile("/sql/etl/view/grantView.sql.st");
 
     public ReportingViewRepository(JdbcTemplate jdbcTemplate) {
@@ -25,39 +25,38 @@ public class ReportingViewRepository  implements ReportingViewMetaData{
     }
 
     @Override
-    public void createOrReplaceView(OrganisationIdentity organisationIdentity){
+    public void createOrReplaceView(OrganisationIdentity organisationIdentity) {
         String schemaName = organisationIdentity.getSchemaName();
         List<String> usersWithSchemaAccess = organisationIdentity.getUsersWithSchemaAccess();
-        createViewAndGrantPermission(Type.INDIVIDUAL, individualViewFile,schemaName,usersWithSchemaAccess);
-        createViewAndGrantPermission(Type.ENROLMENT, enrolmentViewFile,schemaName,usersWithSchemaAccess);
+        createViewAndGrantPermission(Type.SUBJECT, subjectViewFile, schemaName, usersWithSchemaAccess);
+        createViewAndGrantPermission(Type.ENROLMENT, enrolmentViewFile, schemaName, usersWithSchemaAccess);
     }
 
-    private void createViewAndGrantPermission(Type type,String viewFileName,String schemaName,List<String> users){
+    private void createViewAndGrantPermission(Type type, String viewFileName, String schemaName, List<String> users) {
         String viewName = getViewName(type);
         ST st = new ST(viewFileName);
-        st.add(SCHEMA_NAME,schemaName);
-        st.add(VIEW_NAME,viewName);
+        st.add(SCHEMA_PARAM_NAME, schemaName);
+        st.add(VIEW_PARAM_NAME, viewName);
         String query = st.render();
         try {
             jdbcTemplate.execute(query);
-            log.info(String.format("%s view created",viewName));
-            users.forEach(user->grantPermissionToView(schemaName,viewName,user));
-        }catch(DataAccessException  exception){
-            log.debug(String.format("Unable to create view %s",viewName),exception);
+            log.info(String.format("%s view created", viewName));
+            users.forEach(user -> grantPermissionToView(schemaName, viewName, user));
+        } catch (DataAccessException exception) {
+            log.debug(String.format("Unable to create view %s", viewName), exception);
         }
     }
 
-
-    private void grantPermissionToView(String schemaName,String viewName,String userName){
+    private void grantPermissionToView(String schemaName, String viewName, String userName) {
         ST st = new ST(grantViewFile);
-        st.add(SCHEMA_NAME,schemaName);
-        st.add(VIEW_NAME,viewName);
-        st.add(USER_NAME,userName);
+        st.add(SCHEMA_PARAM_NAME, schemaName);
+        st.add(VIEW_PARAM_NAME, viewName);
+        st.add(USER_PARAM_NAME, userName);
         String query = st.render();
         try {
             jdbcTemplate.execute(query);
-        }catch (DataAccessException exception){
-            log.debug(String.format("Unable to grant permission of %s to %s",viewName,userName),exception);
+        } catch (DataAccessException exception) {
+            log.debug(String.format("Unable to grant permission of %s to %s", viewName, userName), exception);
         }
     }
 }
