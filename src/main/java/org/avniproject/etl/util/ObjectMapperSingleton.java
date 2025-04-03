45940352e7ab2df013f85c8c3cf9081b1224f6da
himeshr <himeshl@samanvayfoundation.org>
2025-04-03 12:28:11 +0530

    avniproject/avni-etl#117 | Modify ObjectMapperSingleton to be inline with Avni-server

--- src/main/java/org/avniproject/etl/util/ObjectMapperSingleton.java
+++ src/main/java/org/avniproject/etl/util/ObjectMapperSingleton.java
@@ -1,17 +1,27 @@
 package org.avniproject.etl.util;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
-@Configuration
-@PropertySource("classpath:application.properties")
-public class ObjectMapperSingleton {
+public final class ObjectMapperSingleton {
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JodaModule())
+            .registerModule(new Jdk8Module())
+            .registerModule(new JavaTimeModule())
+            .registerModule(new ParameterNamesModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
-    @Bean
-    public JodaModule jodaModule() {
-        JodaModule module = new JodaModule();
-        return module;
+    private ObjectMapperSingleton() {
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        return objectMapper;
     }
 }
